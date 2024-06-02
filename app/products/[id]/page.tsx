3e03bfbc64207ac5eaa4b76f210451719a4dec08
nullparrot <github@weist.dev>
2024-06-01 19:34:12 -0700

    Display review or no reviews

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -10,24 +10,43 @@ export const metadata: Metadata = {
 };
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const product = await fetchProductById(params.id)
-  return <div className="flex w-full flex-col md:col-span-2">
-    <Image
-        src={product.image_url}
-        alt={`Picture of ${product.name}`}
-        className="mr-4"
-        width={512}
-        height={512}
-      />
-      <div>
-      <h2 className={`${lusitana.className} text-xl md:text-2xl`}>{product.name}</h2>
-      <h3 className={`${lusitana.className} mb-2 text-l`}>${product.price}</h3>
-      <p><strong>Seller: </strong><Link href={`../sellers/${product.artisan_id}`}>{product.fname} {product.lname}</Link></p>
-      <p><strong>Description: </strong>{product.description}</p>
-      <p><strong>Category: </strong>{product.category}</p>
-      </div>
-      <div>
+  const product = await fetchProductById(params.id);
+  return (
+    <div className="flex w-full flex-col items-center">
+      <div className='max-w-screen-md '>
+      <section className='flex flex-col md:flex-row py-4'>
+        <Image
+          src={product.image_url}
+          alt={`Picture of ${product.name}`}
+          className="mr-4"
+          width={512}
+          height={512}
+        />
+        <div>
+          <h2 className={`${lusitana.className} text-xl md:text-2xl`}>
+            {product.name}
+          </h2>
+          <h3 className={`${lusitana.className} text-l mb-2`}>
+            ${product.price}
+          </h3>
+          <p>
+            <strong>Seller: </strong>
+            <Link href={`../sellers/${product.artisan_id}`}>
+              {product.fname} {product.lname}
+            </Link>
+          </p>
+          <p>
+            <strong>Description: </strong>
+            {product.description}
+          </p>
+          <p>
+            <strong>Category: </strong>
+            {product.category}
+          </p>
+        </div>
+      </section>
         <ProductReviews id={params.id} />
-      </div>
-    </div>;
-  }
+        </div>
+    </div>
+  );
+}
--- app/ui/products/reviews.tsx
+++ app/ui/products/reviews.tsx
@@ -1,10 +1,69 @@
 import { fetchReviewsByProductId } from '@/app/lib/data';
+import { lusitana } from '@/app/ui/fonts';
+import { StarIcon as BlankStar, CodeBracketIcon } from '@heroicons/react/24/outline';
+import { StarIcon as FullStar } from '@heroicons/react/24/solid';
 
 
 export default async function ProductReviews({id}:{id: string}){
     const reviews = await fetchReviewsByProductId(id)
-    console.log(reviews)
-    return <div>
-        <p>Reviews Blank {id}</p>
+    console.log(reviews.length)
+    if (reviews.length < 1){
+        return <div className='w-full p-4 divide-y divide-solid divide-black'>
+        <h2 className={`${lusitana.className} text-xl md:text-2xl`}>Product Reviews</h2>
+        <div divide-y divide-solid divide-grey-700>
+            <p>No Reviews</p></div></div>
+    }
+    return <div className='w-full p-4 divide-y divide-solid divide-black'>
+        <h2 className={`${lusitana.className} text-xl md:text-2xl`}>Product Reviews</h2>
+        <div divide-y divide-solid divide-grey-700>
+
+        {
+            reviews.map((review, i) => {
+                switch(review.rating) {
+                    case 1:{
+                        var rating = [1,0,0,0,0];
+                        break;
+                    }
+                    case 2:{
+                        var rating = [1,1,0,0,0];
+                        break;
+                    }
+                    case 3:{
+                        var rating = [1,1,1,0,0];
+                        break;
+                    }
+                    case 4:{
+                        var rating = [1,1,1,1,0];
+                        break;
+                    }
+                    case 5:{
+                        var rating = [1,1,1,1,1];
+                        break;
+                    }
+                    default:{
+                        var rating = [0,0,0,0,0];
+                        break;
+                    }
+                }
+                return (
+                    <div key={review.id} className='p-2'>
+                        <div className='flex flex-row'>
+                        {
+                            rating.map((rate, i) => {
+                                if (rate == 1){
+                                    return <FullStar key={i} className='w-5' />
+                                } else{
+                                    return <BlankStar key={i} className='w-5' />
+                                }
+                            })
+                        }</div>
+                        <p className='font-bold'>{review.user_name}</p>
+                        <p>{review.comment}</p>
+                    </div>
+                )
+            })
+        }
+        </div>
+        
     </div>
 }
